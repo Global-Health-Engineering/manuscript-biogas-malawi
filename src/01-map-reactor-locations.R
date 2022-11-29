f9c2f4af059d3afeeb492907f6aaa6961d009005
Lars Schöbitz <lars@lse.de>
2022-11-29 10:50:15 +0100

    Prepare static map

--- src/01-map-reactor-locations.R
+++ src/01-map-reactor-locations.R
@@ -11,6 +11,8 @@ library(tidyr)
 library(leaflet)
 library(mapview)
 library(ggmap)
+library(sf)
+library(ggplot2)
 
 ## read data
 
@@ -30,39 +32,66 @@ locations_map <- leaflet(locations_tidy) %>%
             zoom = 8) %>% 
     addTiles() %>% 
     addCircleMarkers(
-        lng = ~long, 
+        lng = ~lon, 
         lat = ~lat,
         label = ~id,
         radius = 6,
         stroke = FALSE,
         fillOpacity = 0.7)
-        #labelOptions = labelOptions(noHide = T,
-        #                            textsize = "8px",
-        #                            direction = "left",
-        #                            offset = c(-10, 0),
-        #                            style = list(
-        #                                "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
-        #                                "border-color" = "rgba(0,0,0,0.5)",
-        #                                "font-style" = "italic"
-        #                            ))) 
+#labelOptions = labelOptions(noHide = T,
+#                            textsize = "8px",
+#                            direction = "left",
+#                            offset = c(-10, 0),
+#                            style = list(
+#                                "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
+#                                "border-color" = "rgba(0,0,0,0.5)",
+#                                "font-style" = "italic"
+#                            ))) 
 #
 
 
 
 locations_map %>% 
-    mapshot(file = "figs/map-biogas-reactors.png")
+    mapshot(file = "figs/map-biogas-reactors-01.png")
 
 
 # ggmap map ---------------------------------------------------------------
 
 locations_tidy
 
-qmplot(x = lon, y = lat, data = locations_tidy, color = I("red"), zoom = 14)
+locations_sf <- st_as_sf(locations_tidy, coords = c("lon", "lat"))
 
-library(spData)
-data("us_states", package = "spData")
+st_crs(locations_sf) <- 4326
 
-install.packages('spDataLarge', repos='https://nowosad.github.io/drat/', type='source')
+#unzip(zipfile = "data/mwi_adm_nso_20181016_shp.zip", exdir = "data/mwi_adm_nso_20181016_shp/")
+
+read_sf("data/mwi_adm_nso_20181016_shp/mwi_admbnda_adm0_nso_20181016.shp") %>% 
+    ggplot() +
+    geom_sf()
+
+read_sf("data/mwi_adm_nso_20181016_shp/mwi_admbnda_adm1_nso_20181016.shp") %>% 
+    ggplot(aes(fill = ADM1_EN)) +
+    geom_sf()
+
+malawi_shp_adm2 <- read_sf("data/mwi_adm_nso_20181016_shp/mwi_admbnda_adm2_nso_20181016.shp") 
+
+malawi_shp_adm2_4326 <- st_transform(malawi_shp_adm2, crs = 4326)
+
+ggplot()  +
+    geom_sf(data = malawi_shp_adm2_4326 ,fill = "white") +
+    geom_sf(data = locations_sf, 
+            color = "steelblue", 
+            alpha = 0.6,
+            size = 3)
+
+ggsave(filename = "figs/map-biogas-reactors-02.png")
 
 
-spData::
+
+# read_sf("data/mwi_adm_nso_20181016_shp/mwi_admbnda_adm3_nso_20181016.shp") %>% 
+#     ggplot(aes(fill = ADM3_EN))  +
+#     geom_sf()
+
+# read_sf("data/mwi_adm_nso_20181016_shp/mwi_admbndl_admALL_nso_itos_20181016.shp") %>% 
+#     ggplot() +
+#     geom_sf(fill = "white")
